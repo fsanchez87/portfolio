1993bd168da609301e12cba79d8660a8a6872e0a
fsanchez87 <sanchezgarridof@gmail.com>
2022-01-10 21:39:20 +0100

    create HTML About component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Skills />
       <Testimonials />
       <Contact />
+      
     </main>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,32 @@
 export default function About() {
-    return (
-        <div>
-            
+  return (
+    <section id="about">
+      <div className="">
+        <div className="">
+          <h1 className="">
+            Hola, soy Francisco Sánchez.
+            <br className="" />I love to build apps.
+          </h1>
+          <p className="">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            ipsum voluptatem perspiciatis laudantium nulla velit, assumenda
+            tempore quod enim illo possimus nisi quis voluptatibus aspernatur
+            dicta ullam architecto expedita provident facere non fuga amet
+            magnam? In est voluptatum possimus natus?
+          </p>
+          <div className="">
+            <a href="#contact" className="">
+              Trabaja conmigo
+            </a>
+            <a href="#projects" className="">
+              Mi trabajo
+            </a>
+          </div>
         </div>
-    )
+        <div className="">
+          <img className="" alt="" src=""></img>
+        </div>
+      </div>
+    </section>
+  );
 }
