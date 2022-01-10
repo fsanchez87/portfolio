8816bf07593277f33af9fd8696f5c333f41d9fc7
fsanchez87 <sanchezgarridof@gmail.com>
2022-01-10 21:22:17 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 
