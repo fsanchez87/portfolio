b8eebda7d59d2ea1382961cd007ccce7e3def649
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-16 20:17:43 +0100

    Build Navbar Componet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 export default function Navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap flex-col p-5 md:flex-row ">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Francisco Sánchez
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Trabajos
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+          <a href="#contact" className="mr-5 hover:text-white">
+            Contacto
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
