a1b5d179ccdf1856a30446a16ec8cf4062d2efb7
Francisco Sánchez <sanchezgarridof@gmail.com>
2025-01-27 08:55:49 +0100

    add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import partytown from '@astrojs/partytown'
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: netlify()
+    integrations: [
+        tailwind(),
+        icon(),
+        compress(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+    output: "server",
+    adapter: netlify(),
 });
